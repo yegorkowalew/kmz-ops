bcbea5122f84cc1101729bf2bcd3c13fd1821d4e
yegorkowalew <kowalew.backup@gmail.com>
2019-04-12 16:22:59 +0300

    Порядок со статикой.

---
+++ ops/static/js/ops.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#dismiss, .overlay').on('click', function () {
+        $('#sidebar').removeClass('active');
+        $('.overlay').removeClass('active');
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').addClass('active');
+        $('.overlay').addClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+    $(".peity-donut").peity("donut");
+
+    $('#flt-ready').click(function() {
+    $(this).toggleClass("btn-success btn-outline-success");
+    $('.ready').toggleClass("show");
+    return false;
+    });
+
+    $('#flt-overdue').click(function() {
+    $(this).toggleClass("btn-danger btn-outline-danger");
+    $('.overdue').toggleClass("show");
+    return false;
+    });
+
+    $('#flt-tendays').click(function() {
+    $(this).toggleClass("btn-warning btn-outline-warning");
+    $('.tendays').toggleClass("show");
+    return false;
+    });
+
+    $('#flt-inwork').click(function() {
+    $(this).toggleClass("btn-primary btn-outline-primary");
+    $('.inwork').toggleClass("show");
+    return false;
+    });
+
+    $('#flt-np').click(function() {
+    $(this).toggleClass("btn-info btn-outline-info");
+    $('.np').toggleClass("show");
+    return false;
+    });
+
+    $.each($('.l-order'), function(i) {
+        if(i % 2 == 0){
+            // console.log(i);
+            $(this).addClass("l-order-two");
+        };
+        
+    });
+});
--- ops/timeworker/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TimeworkerConfig(AppConfig):
-    name = 'timeworker'
-    verbose_name = 'Воркер'
